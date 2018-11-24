c76d73a2a8db09e016ff7461d2b3f7308a61bc3b
Preston Wallace <wallace.preston@gmail.com>
2018-11-24 00:06:04 -0800

    finished specs on cycle-1

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,41 @@
+const Student = require('../db/models/student')
 const router = require('express').Router();
+
+router.get('/', async(req, res, next) => {
+    const allStudents = await Student.findAll()
+    res.send(allStudents)
+})
+
+router.get('/:id', async(req, res, next) => {
+    const id = req.params.id
+    const oneStudent = await Student.findById(id)
+    oneStudent ? 
+        res.send(oneStudent) : 
+        res.status(404).send('404 Not Found')
+})
+
+router.post('/', async (req, res, next) => {
+    Student.create(req.body);
+    res.status(201);
+    res.send(req.body)
+})
+
+router.put('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    const [numAffectedRows, student] = await Student.update(req.body,{
+        where: {id},
+        returning: true,
+        plain: true
+    });
+    res.send(student)
+})
+
+router.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    await Student.destroy({where: {id}})
+    res.status(204);
+    res.send();
+})
 
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,32 @@
+const Test = require('../db/models/test');
 const router = require('express').Router();
 
+router.get('/', async (req, res, next) => {
+    const allTests = await Test.findAll();
+    res.send(allTests);
+})
 
+router.get('/:id', async (req, res, next) => {
+    const instance = await Test.findById(req.params.id);
+    res.send(instance)
+})
+
+router.post('/student/:studentId', async (req, res, next) => {
+    let testToAdd = req.body;
+    testToAdd.studentId = req.params.studentId;
+    const newTest = await Test.create(testToAdd)
+    res.status(201)
+    res.send(newTest)
+})
+
+
+router.delete('/:id', async (req, res, next) => {
+    const numAffectedRows = await Test.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(204)
+    res.send(await Test.findById(req.params.id))
+})
 module.exports = router;
